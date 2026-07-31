5d3d403e9b747b1cfe4554a41889810c58064312
RuDr8A <rudrasingh240310@gmail.com>
2026-08-01 01:12:30 +0530

    Add user logic logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 async function registerUser(req, res) {
     const {username, email, password, role = 'user'} = req.body ;
@@ -15,10 +16,12 @@ async function registerUser(req, res) {
         return res.status(409).json({message : "user already exists"}) ;
     }
 
+    const hash = await bcrypt.hash(password, 10) ;
+
     const newUser = await userModel.create({
         username,
         email,
-        password,
+        password : hash,
         role
     })
 
@@ -34,4 +37,34 @@ async function registerUser(req, res) {
     })
 }
 
-module.exports = registerUser ;
+async function userLogin(req, res){
+    const {username, email, password} = req.body ;
+    const user = await userModel.findOne({
+        $or : [
+            {username},
+            {email}
+        ]
+    })
+
+    if(!user){
+        return res.status(401).json({message : "Invalid Credentials"}) ;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if(!isPasswordValid){
+        return res.status(401).json({message : "Invalid Credentials"}) ;
+    }
+    const token = jwt.sign({
+        _id : user._id,
+        role : user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token) ;
+    res.status(200).json({
+        message : "user logged in successfully",
+        User : user
+    })
+
+}
+
+module.exports = {registerUser, userLogin} ;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const registerUser = require('../controllers/auth.controller');
+const authController = require('../controllers/auth.controller');
 
 
 const router = express.Router() ;
 
-router.post('/register',registerUser)
+router.post('/register',authController.registerUser)
+
+router.post('/login',authController.userLogin)
 
 
 
